1110da13402ca5cccce7467b6e5dbe6ef75d6a22
Akshit Patel <aksh1150@gmail.com>
2020-11-22 15:51:17 -0500

    Update

--- dbserver/api/advisor/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/getAdvisors/', require('./getAdvisors'));
-router.put('/updateAdvisorId/', require('./updateAdvisorId'));
-
-module.exports = router;
--- dbserver/api/user/index.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/info/', require('./info/'));
-
-module.exports = router;
