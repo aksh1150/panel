032d51abc66b79743effe203119c65b824f43261
Akshit Patel <aksh1150@gmail.com>
2020-11-22 20:25:28 -0500

    Update

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -9,6 +9,7 @@
 
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import HomePage from '../HomePage/Loadable';
 import UserInfo from '../UserInfo/Loadable';
 import AddContact from '../AddContact/Loadable';
@@ -19,7 +20,7 @@ import ShowAllCases from '../ShowAllCases/Loadable';
 import ShowAllLeads from '../ShowAllLeads/Loadable';
 import EditCase from '../EditCase/Loadable';
 import EditLead from '../EditLead/Loadable';
-import EditContact from '../EditContact/Loadable';
+import EditContact from '../editContact/Loadable';
 import ForgotPassword from '../ForgotPassword/Loadable';
 import NotFoundPage from '../NotFoundPage/Loadable';
 import SignUp from '../SignUp/Loadable';
